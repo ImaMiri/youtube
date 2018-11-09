6ae8ce7c661e4d635dc9ebf1315eabbaa61b7f22
Ima <ima.techkube@gmail.com>
2018-11-09 20:31:12 +1100

    Initial Commit

---
+++ youtube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube
+//
+//  Created by Ima Miri on 9/11/18.
+//  Copyright © 2018 TechKube. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
